1589938e1b8980826b4ac5373831f22f6a420483
Daavidmp <cod.davidmp@gmail.com>
2026-02-08 19:13:43 +0100

    Actualización del mensaje para el examen

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola Profe Manu\n');
+    res.end('Hola Profe Manu, actualizado por David\n');
 });
 server.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
